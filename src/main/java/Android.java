3dd9aa3c942797ced9fb6fde650a8e894460dd8e
unknown <ajaykumar@DLLB0401.qait.com>
2015-02-05 17:07:51 +0530

    Sharing Appium Demo

---
+++ src/main/java/Android.java
@@ -0,0 +1,15 @@
+package com.demo.appium;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class AndroidTest {
+}
